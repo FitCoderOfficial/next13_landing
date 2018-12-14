835a76c25741885c5d9e9b2cde1435a694945899
SaeByeok Shin <toqur1219@naver.com>
2018-12-15 00:11:50 +0900

    feat: Add css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -16,7 +17,7 @@ class App extends Component {
 
 
     return (
-      <div>
+      <div class="my-div">
       <h1> 리액트 안녕하세요! </h1>
       <h2> 리액트를 {text} 처음개발했습니다 </h2>
       {
