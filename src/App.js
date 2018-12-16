cca1078d71c581c369ec25b0e9ea54c2d51b7da2
SaeByeok Shin <toqur1219@naver.com>
2018-12-16 20:32:29 +0900

    feats: Add new component
    This reverts commit 60fa648c4c01e77a423c25878721c9ed6b2b849a.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
@@ -18,22 +17,13 @@ class App extends Component {
 
 
     return (
-      
-      <div className="my-div"> 
-      <MyComponent/>
+      <div class="my-div">
       <h1> 리액트 안녕하세요! </h1>
       <h2> 리액트를 {text} 처음개발했습니다 </h2>
       {
          condition && '보여주세요'
-         
       }
-      <div style={style}
-      // 셀프 클로즈 태그에서만 작동하는 주석
-      // 마지막 />가 꼭 새 줄에 있어야 합니다.
-      /* 이렇게 작성할 수 있습니다 */
-      />
-      //이렇게 쓰는 것은 렌더링됩니다
-      /* 여기에서는 주석을 못써요 */
+      <div style={style}></div>
       </div>
     );
   }
