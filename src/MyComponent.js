7bf4422a4b6336af490e5288a22c314874f36d0a
SaeByeok Shin <toqur1219@naver.com>
2018-12-20 10:58:42 +0900

    feat: state update

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -21,7 +21,8 @@ class MyComponent extends Component {
     return (
       <div>
         <p>안녕하세요, 제 이름은 { this.props.name }입니다.</p>
-        <p>제 나이는 { this.state.number }살 입니다.</p>
+        <p>제 나이는 { this.props.age }살 입니다.</p>
+        <p> 숫자 : {this.state.number} </p>
       </div>
     )
   }
